f74d6fe13f9fd9cd93fea9ed018d3de05b88159a
toshokan <toshokan@shojigate.net>
2021-02-27 10:10:18 -0500

    fmt benchmark

--- benches/parses_own_output.rs
+++ benches/parses_own_output.rs
@@ -1,5 +1,5 @@
-use misato::proto::wire::*;
 use misato::proto::encode::write;
+use misato::proto::wire::*;
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
@@ -20,28 +20,71 @@ fn make_header() -> (&'static str, MessageHeader<'static>) {
     (input, exp)
 }
 
-fn parse_and_reencode(c: &mut Criterion) {
-    let (input, exp) = make_header();
-    let bytes = input.as_bytes();
-    let mut buf = vec![];
-    
-    c.bench_function("parse_and_reencode", |b| b.iter(|| {
-	let mut parser = Parser::new(bytes);
-	let (_, header) = parser.parse_header(bytes).unwrap();
-	
-	let msg = Message {
-	    header,
-	    body: vec![]
-	};
-	
-	write(&mut buf, msg).unwrap();
-
-	buf.clear();
-	
-	()
-    }));
+fn data() -> Vec<Data<'static>> {
+    vec![
+        Data::String("The quick brown fox will probably never stop jumping over the lazy dog"),
+        Data::ObjectPath("/var/log/auth.log"),
+        Data::UInt32(12312),
+        Data::Boolean(true),
+        Data::String("alalalalalalallajahdsuh"),
+        Data::Double(3.14159),
+    ]
 }
 
+fn parse_only(c: &mut Criterion) {
+    let (input, exp) = make_header();
+    let bytes = input.as_bytes();
 
-criterion_group!(benches, parse_and_reencode);
+    c.bench_function("parse_only", |b| {
+        b.iter(|| {
+            let mut parser = Parser::new(bytes);
+            let (_, output) = parser.parse_header(bytes).unwrap();
+            ()
+        })
+    });
+}
+
+fn encode_only(c: &mut Criterion) {
+    let (_, exp) = make_header();
+    let mut buf = vec![];
+
+    let msg = Message {
+        header: exp,
+        body: data(),
+    };
+
+    c.bench_function("encode_only", |b| {
+        b.iter(|| {
+            write(&mut buf, &msg).unwrap();
+
+            ()
+        })
+    });
+}
+
+fn parse_and_reencode(c: &mut Criterion) {
+    let (input, _) = make_header();
+    let bytes = input.as_bytes();
+    let mut buf = vec![];
+
+    c.bench_function("parse_and_reencode", |b| {
+        b.iter(|| {
+            let mut parser = Parser::new(bytes);
+            let (_, output) = parser.parse_header(bytes).unwrap();
+
+            let msg = Message {
+                header: output,
+                body: vec![],
+            };
+
+            write(&mut buf, &msg).unwrap();
+
+            buf.clear();
+
+            ()
+        })
+    });
+}
+
+criterion_group!(benches, parse_and_reencode, parse_only, encode_only);
 criterion_main!(benches);
